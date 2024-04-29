76afa5c154c108f61280fa10a476b4a6a93671f3
Aristotel <124895720+simeonidi03@users.noreply.github.com>
2024-04-29 17:23:35 +0300

    Initial commit

---
+++ control/Debug/user/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../user/at32f403a_407_clock.c \
+../user/at32f403a_407_int.c \
+../user/i2c_application.c \
+../user/main.c 
+
+OBJS += \
+./user/at32f403a_407_clock.o \
+./user/at32f403a_407_int.o \
+./user/i2c_application.o \
+./user/main.o 
+
+C_DEPS += \
+./user/at32f403a_407_clock.d \
+./user/at32f403a_407_int.d \
+./user/i2c_application.d \
+./user/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+user/%.o: ../user/%.c user/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -ffunction-sections  -g -DAT_START_F403A_V1 -DAT32F403AVGT7 -DUSE_STDPERIPH_DRIVER -I"../include" -I"../include/libraries/drivers/inc" -I"../include/libraries/cmsis/cm4/core_support" -I"../include/libraries/cmsis/cm4/device_support" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
